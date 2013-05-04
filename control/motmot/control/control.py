48467362bc4950a5bc049b149f4daedab6be5b81
JLab <jlab@jlab-System-Product-Name.(none)>
2013-05-04 12:13:08 +0100

    Got fview plugin working

--- control/motmot/control/control.py
+++ control/motmot/control/control.py
@@ -7,8 +7,8 @@ import motmot.fview.traited_plugin as traited_plugin
 import traits.api as traits
 import numpy as np
 from traitsui.api import View, Item, Group, FileEditor
-from multiprocessing import Process, Queue, Manager
-#from threading import Thread
+#from multiprocessing import Process, Queue, Manager
+from threading import Thread
 #from Queue import Queue
 
 if 1:
@@ -26,56 +26,24 @@ SERVER_ENDPOINT = "tcp://localhost:5556"
 # in a separate process, so that it can delay, etc.
 # without messing up the video acquisition
 # Also, have it deal with the logging.
-class ODDControlWorker(Process):
-  def __init__(self, task_q):
-    Process.__init__(self)
-    self.task_q = task_q
-    self.save_to_disk = False
-    self.log_file_name = ""
-    self.log_file = None
+class ODDControlWorker(Thread):
+  def __init__(self, odor, stim, delay, client, poll):
+    Thread.__init__(self)
+    self.odor = odor
+    self.stim = stim
+    self.delay = delay
+    self.client = client
+    self.poll = poll
     return
 
  # pass tuples (task_name, task_args) where task_args is a dict
   def run(self):
-    self.context = zmq.Context(1)
+    self.send_odor_command(self.odor, self.stim, self.delay)
 
-    self.client = self.context.socket(zmq.REQ)
-    self.client.connect(SERVER_ENDPOINT)
-
-    self.poll = zmq.Poller()
-    self.poll.register(self.client, zmq.POLLIN)
-
-#    time.sleep(1)
-
-    while True:
-      time.sleep(1)
-      print "Hello"
-#      next_task_name, next_task_args = self.task_q.get()
-#      if next_task_name == "quit":
-#        self.context.term()
-#        break
-#      else:
-#        task = getattr(self, next_task_name)
-#        result = task(**next_task_args)
-
-  def open_log_file(self, log_fname):
-    self.save_to_disk = True
-    self.log_file = open(log_fname, 'w')
-    self.log_file_name = log_fname
-
-  def close_log_file(self):
-    self.save_to_disk = False
-    self.log_file_name = ''
-    self.log_file.close()
-    self.log_file = None
-
-  def send_odor_command(self, odor_name, odor, stim, delay, timestamp, framenumber):
+  def send_odor_command(self, odor, stim, delay):
     blank = int(8*int(math.floor(odor/8)))
     request = "%d %d %d %d " % (odor, stim, delay, blank)
     self.client.send(request)
-    if self.save_to_disk:
-      loginfo = "S %s %d %d %d %d %d %d %d\n" % (odor_name, odor, stim, delay, blank, timestamp, framenumber, time.mktime(time.gmtime()))
-      self.log_file.write(loginfo)
     time.sleep(stim + delay)
     expect_reply = True
     while expect_reply:
@@ -85,9 +53,6 @@ class ODDControlWorker(Process):
         if not reply:
           break
         else:
-          if self.save_to_disk:
-            loginfo = "R %s %d %d %d %d %d %d %d\n" % (odor_name, odor, stim, delay, blank, timestamp, framenumber, time.mktime(time.gmtime()))
-            self.log_file.write(loginfo)
           expect_reply = False
 
 
@@ -108,6 +73,7 @@ def read_odd_file(fname):
     delay = int(elems[2])
     stim = int(elems[3])
     odors.append((name, odor, delay, stim))
+  odors.reverse() # so pops come off in the right order
   return odors
 
 # design: 
@@ -121,8 +87,9 @@ class OdorControl(traited_plugin.HasTraits_FViewPlugin):
   log_filename = traits.File
   odd_filename = traits.File
   persistent_attr_names = traits.List(['odd_filename', 'log_filename'])
-
+  deliver_odors = traits.Bool(False, transient=True)
   traits_view = View(Group(\
+    Item(name="deliver_odors"),\
     Item(name='save_to_disk'),\
     Item(name="odd_filename", editor=FileEditor(dialog_style="open")),\
     Item(name="log_filename_prefix"),\
@@ -130,69 +97,88 @@ class OdorControl(traited_plugin.HasTraits_FViewPlugin):
 
   def __init__(self, *args, **kwargs):
     super(OdorControl,self).__init__(*args, **kwargs)
+    print "Initialized odor control"
     self._list_of_timestamp_data = []
     self.odors = []
     self.last_trigger_timestamp = {}
     self.sending_odor = False
-    self.delay = 0
+    self.total_delay = 0
     self.has_started = False
+    self.log_file = None
+    self.worker = None
+    self.context = zmq.Context(1)
+    self.client = self.context.socket(zmq.REQ)
+    self.client.connect(SERVER_ENDPOINT)
+    self.poll = zmq.Poller()
+    self.poll.register(self.client, zmq.POLLIN)
     print "Socket initialized on port 5556..."
+
     self.has_started = True
-    self.manager = Manager()
-    self.q = self.manager.Queue()
-    self.worker = ODDControlWorker(self.q)
-    print "Worker process created..."
-    self.worker.daemon = True
-    self.worker.start()
-    print "Worker process initialized..."
+
+    # variables for current odor
+    self.odor_name = ""
+    self.odor = 0
+    self.stim = 0
+    self.delay = 0
 
   def _save_to_disk_changed(self):
     print "Changing save to disk"
     if self.has_started:
       if self.save_to_disk:
         self.log_filename = self.log_filename_prefix + time.strftime('%Y%m%d_%H%M%S') + ".txt"
-        self.q.put(("open_log_file", {"log_fname":self.log_filename}))
+        self.log_file = open(self.log_filename, 'w')
         print "Saving ", repr(self.log_filename), " to disk..."
       else:
         print "Closed", repr(self.log_filename)
-        self.q.put(("close_log_file", {}))
+        self.log_file.close()
+        self.log_file = None
         self.log_filename = ""
 
   def _odd_filename_changed(self):
     print "Changing ODD filename"
     try:
-      print "Using ODD file ", repr(self.odd_filename)
-      self.odors = read_odd_file(repr(self.odd_filename))
+      print "Using ODD file ", self.odd_filename
+      self.odors = read_odd_file(self.odd_filename.encode('ascii'))
     except IOError:
-      print "Couldn't open ODD file ", repr(self.odd_filename)
+      print "Couldn't open ODD file ", self.odd_filename
 
   def quit(self):
-    if self.has_started:
-      self.q.put(("quit", {}))
+    pass
+    if self.has_started and self.worker is not None:
       self.worker.join()
 
   def camera_starting_notification(self, cam_id, pixel_format=None, max_width=None, max_height=None):
+    self.has_started = True
     pass
 
   def process_frame(self, cam_id, buf, buf_offset, timestamp, framenumber):
-    if framenumber == 0:
-      self.odors = read_odd_file("/home/jlab/odors.txt")
-      print self.odors
     draw_points = []
     draw_linesegs = []
-
     now = time.time()
-    if len(self.odors) > 0:
+    if self.deliver_odors:
       if not self.sending_odor:
-        print "Sending odor ", o[0]
-        o = self.odors.pop()
-        self.delay = o[1] + o[2]
-        q.put(("send_odor_command", {"odor_name":o[0], "odor":o[1], "stim":o[2], "delay":o[3], "timestamp":timestamp, "framenumber":framenumber}))
+        self.odor_name, self.odor, self.stim, self.delay = self.odors.pop()
+        if self.save_to_disk:
+          loginfo = "S %s %d %d %d %d %d %d\n" % (self.odor_name, self.odor, self.stim, self.delay, framenumber, time.mktime(time.gmtime()), timestamp)
+          self.log_file.write(loginfo)
+        self.total_delay = self.stim + self.delay
+        print "Sending odor ", self.odor_name, " with total delay ", self.total_delay
+        self.worker = ODDControlWorker(self.odor, self.stim, self.delay, self.client, self.poll)
+        self.worker.daemon = True
+        self.worker.start()
         self.sending_odor = True
         self.last_update_time = time.time()
       else:
-        if (self.last_update_time - now) > self.delay:
+        if (now - self.last_update_time) > self.total_delay:
+          print "Changing odor from ", self.odor_name
+          self.worker.join()
           self.sending_odor = False
-    self.last_trigger_timestamp[cam_id] = timestamp
+          if self.save_to_disk:
+            loginfo = "R %s %d %d %d %d %d %d\n" % (self.odor_name, self.odor, self.stim, self.delay, framenumber, time.mktime(time.gmtime()), timestamp)
+            self.log_file.write(loginfo)
+          if len(self.odors) == 0:
+            self.deliver_odors = False
+            # reload odors
+            self.odors = read_odd_file(self.odd_filename.encode('ascii'))
     return draw_points, draw_linesegs
 
